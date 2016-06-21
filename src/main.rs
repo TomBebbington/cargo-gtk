a8063553f05ab9deccc05296b22e1ef88e84abc9
Tom Bebbington <tophattedcoder@gmail.com>
2016-06-22 00:31:55 +0100

    Add license and improve source code readability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+/*
+Copyright 2016 Tom Bebbington
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 extern crate gtk;
 extern crate thread_scoped;
 extern crate cargo;
@@ -63,23 +78,34 @@ macro_rules! error {
     ($window:expr, $($tt:tt)*) => (error($window, &format!($($tt)*)))
 }
 
-pub struct Options<'a, F> where F: Fn() -> CompileFilter<'a> + 'a {
+#[derive(Clone)]
+pub enum Filter {
+    Everything
+}
+impl<'a> Into<CompileFilter<'a>> for &'a Filter {
+    fn into(self) -> CompileFilter<'a> {
+        match self {
+            &Filter::Everything => CompileFilter::Everything
+        }
+    }
+}
+
+pub struct Options {
     pub config: Config,
     pub jobs: Option<u32>,
     pub target: Option<String>,
     pub features: Vec<String>,
     pub no_default_features: bool,
     pub spec: Vec<String>,
-    pub filter: F,
+    pub filter: Filter,
     pub exec_engine: Option<Arc<Box<ExecEngine>>>,
     pub release: bool,
     pub mode: CompileMode,
     pub target_rustdoc_args: Option<Vec<String>>,
-    pub target_rustc_args: Option<Vec<String>>,
-    data: PhantomData<& 'a ()>
+    pub target_rustc_args: Option<Vec<String>>
 }
-impl<'a, F> Options<'a, F> where F: Fn() -> CompileFilter<'a> + 'a {
-    pub fn new(filter: F) -> Options<'a, F> where F: Fn() -> CompileFilter<'a> + 'a {
+impl Default for Options {
+    fn default() -> Options {
         Options {
             jobs: Some(8),
             features: Vec::new(),
@@ -87,17 +113,16 @@ impl<'a, F> Options<'a, F> where F: Fn() -> CompileFilter<'a> + 'a {
             config: Config::default().unwrap(),
             exec_engine: None,
             release: false,
-            filter: filter,
+            filter: Filter::Everything,
             no_default_features: false,
             target: None,
             target_rustdoc_args: None,
             target_rustc_args: None,
-            mode: CompileMode::Build,
-            data: PhantomData
+            mode: CompileMode::Build
         }
     }
 }
-impl<'a, F> Into<CompileOptions<'a>> for &'a Options<'a, F> where F: Fn() -> CompileFilter<'a> + 'a {
+impl<'a> Into<CompileOptions<'a>> for &'a Options {
     fn into(self) -> CompileOptions<'a> {
         CompileOptions {
             config: &self.config,
@@ -106,7 +131,7 @@ impl<'a, F> Into<CompileOptions<'a>> for &'a Options<'a, F> where F: Fn() -> Com
             features: &self.features,
             no_default_features: self.no_default_features,
             spec: &self.spec,
-            filter: (self.filter)(),
+            filter: (&self.filter).into(),
             exec_engine: self.exec_engine.clone(),
             release: self.release,
             mode: self.mode,
@@ -136,6 +161,7 @@ pub struct Context {
     pub local_description: Label,
     pub online_search: SearchEntry,
     pub online_packages: TreeView,
+    pub options: Rc<Options>
 }
 
 fn update_labels(labels: &[(&Label, &str)]) {
@@ -158,6 +184,7 @@ impl Context {
         packs.append_column(&make_column("Version", "text", 2));
         Context {
             config: Rc::new(Config::default().unwrap()),
+            options: Rc::new(Options::default()),
             window: builder.get_object("window").unwrap(),
             file: builder.get_object("file").unwrap(),
             build: builder.get_object("build").unwrap(),
@@ -177,14 +204,17 @@ impl Context {
             online_packages: packs
         }
     }
-    fn bind_button<A>(&self, name: &'static str, button: &Button, action: A) where A: Fn(&Path) + 'static {
+    fn bind_button<F>(&self, name: &'static str, button: &Button, mods: F) where F: Fn(&mut CompileOptions) + 'static {
         let file = self.file.clone();
         let window = self.window.clone();
+        let options = self.options.clone();
         button.connect_clicked(move |_| {
             if let Some(file) = file.get_filename() {
-                action(&file);
-            } else {
-                error!(Some(&window), "A Cargo.toml package must be selected to run the '{}' command", name);
+                let mut options: CompileOptions = (&*options).into();
+                mods(&mut options);
+                if let Err(err) = ops::compile(&file, &options) {
+                    error!(Some(&window), "Failed to {} '{:?}': {:?}", name, file, err);
+                }
             }
         });
     }
@@ -213,7 +243,6 @@ impl Context {
         let packs2 = self.online_packages.clone();
         let (tx, rx) = channel();
         let thread = RefCell::new(None);
-        let file = self.file.clone();
 
 
         self.online_search.connect_activate(move |entry| {
@@ -226,47 +255,44 @@ impl Context {
                 tx.send(registry.search(&query, 64).map_err(|_| "Search failed").unwrap().0).unwrap();
             })});
         });
+        self.bind_button("build", &self.build, |_| {});
+        self.bind_button("test", &self.test, |mut c| c.mode = CompileMode::Test);
+        self.bind_button("bench", &self.bench, |mut c| c.mode = CompileMode::Bench);
         let window = self.window.clone();
-        let config2 = self.config.clone();
+        let config = self.config.clone();
+        let options = self.options.clone();
         self.install.connect_clicked(move |_| {
             let index = packs2.get_cursor().0.unwrap().get_indices()[0];
             let results = results.borrow();
-            let id = SourceId::for_central(&*config2).unwrap();
-            let options = Options::new(|| CompileFilter::Everything);
-            let options: CompileOptions = (&options).into();
+            let id = SourceId::for_central(&*config).unwrap();
+            let options: CompileOptions = (&*options).into();
             let name = &results[index as usize].name;
             if let Err(err) = ops::install(None, Some(name), &id, None, &options) {
                 error!(Some(&window), "Failed to install '{}': {:?}", name, err);
             }
         });
-        let window2 = self.window.clone();
+        let window = self.window.clone();
+        let file = self.file.clone();
+        let options = self.options.clone();
         self.run.connect_clicked(move |_| {
             if let Some(file) = file.get_filename() {
-                let options = Options::new(|| CompileFilter::Everything);
-                let options: CompileOptions = (&options).into();
+                let options: CompileOptions = (&*options).into();
                 if let Err(err) = ops::run(&file, &options, &[]) {
-                    error!(Some(&window2), "Failed to run '{:?}': {:?}", file, err);
+                    error!(Some(&window), "Failed to run '{:?}': {:?}", file, err);
                 }
             }
         });
         let window = self.window.clone();
-        let config3 = self.config.clone();
-        self.bind_button("publish", &self.publish, move |path| {
-            let package = Package::for_path(&path, &*config3).unwrap();
-            if !package.publish() {
-                error(Some(&window), "Failed to publish crate");
+        let file = self.file.clone();
+        let config = self.config.clone();
+        self.publish.connect_clicked(move |_| {
+            if let Some(file) = file.get_filename() {
+                let package = Package::for_path(&file, &*config).unwrap();
+                if !package.publish() {
+                    error(Some(&window), "Failed to publish crate");
+                }
             }
         });
-        /*
-        self.bind_buttons(&[
-            (&self.build, "cargo build"),
-            (&self.run, "cargo run"),
-            (&self.bench, "cargo bench"),
-            (&self.publish, "cargo publish"),
-            (&self.update, "cargo update"),
-            (&self.doc, "cargo doc"),
-            (&self.test, "cargo test")
-        ]);*/
         let self2 = self.clone();
         let old_file = RefCell::new(self.file.get_filename());
         let file = self.file.clone();
